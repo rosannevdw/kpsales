f40e41afb679e1009591f061c14f419e9802ba9f
Rosannevdw <rvandeweerdt@gmail.com>
2026-09-11 09:44:33 +0200

    changed inFocus to medicalTeam

--- models/clinic.ts
+++ models/clinic.ts
@@ -2,8 +2,8 @@
 import clientJourney from "../data/clinic/client-journey.json" with { type: "json" };
 import aboutUs from "../data/clinic/about-the-practice.json" with { type: "json" };
 import investment from "../data/clinic/investment-inclusions.json" with { type: "json" };
-import inFocus from "../data/clinic/in-focus.json" with { type: "json" };
+import medicalTeam from "../data/clinic/medical-team.json" with { type: "json" };
 
-export const clinic = [clientJourney, aboutUs, investment, inFocus]
+export const clinic = [clientJourney, aboutUs, investment, medicalTeam]
   .sort(c => c.order).map(c =>
   ({...c, slug: c.title.replaceAll(" ", "-").toLowerCase().replace(/[^a-z0-9]/g, "")}));
