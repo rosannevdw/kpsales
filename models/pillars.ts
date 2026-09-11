82ca19431995a173ecd62b4c654c6033128d7c70
Rosannevdw <rvandeweerdt@gmail.com>
2026-09-11 09:36:42 +0200

    added missing bio-r pillar file

--- models/pillars.ts
+++ models/pillars.ts
@@ -1,7 +1,7 @@
 
 import mentalHealth from "../data/pillars/mental-health.json" with { type: "json" };
 import internalMedicine from "../data/pillars/internal-medicine.json" with { type: "json" };
-import bioR from "../data/pillars/bioRmethod.json" with { type: "json" };
+import bioR from "../data/pillars/biormethod.json" with { type: "json" };
 import hospitality from "../data/pillars/hospitality.json" with { type: "json" };
 
 export const pillars = [mentalHealth, internalMedicine, bioR, hospitality]
